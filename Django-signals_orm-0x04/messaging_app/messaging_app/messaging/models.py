5454476672ebaab45e901d67c71ee2002210fa18
Nyakio-Eva <evalkariuki@gmail.com>
2025-08-02 11:35:51 +0300

    Create a Signal for Logging Message Edits

--- Django-signals_orm-0x04/messaging_app/messaging_app/messaging/models.py
+++ Django-signals_orm-0x04/messaging_app/messaging_app/messaging/models.py
@@ -11,6 +11,7 @@ class Message(models.Model):
 
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    edited = models.BooleanField(default=False) 
 
     def __str__(self):
         return f"From {self.sender} to {self.receiver} at {self.timestamp}"
@@ -24,3 +25,11 @@ class Notification(models.Model):
 
     def __str__(self):
         return f"Notification for {self.user} about message {self.message.id}"
+
+class MessageHistory(models.Model):
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='history')
+    old_content = models.TextField()
+    edited_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"History of Message {self.message.id} at {self.edited_at}"
